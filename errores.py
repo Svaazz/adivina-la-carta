6a1a07564fb159b72e1fe0965fb658ac6bf03f7c
Mike <Svaazz@users.noreply.github.com>
2015-12-09 17:45:48 +0100

    Create errores.py

---
+++ errores.py
@@ -0,0 +1,11 @@
+
+def error(numero):
+	if numero == 1:
+		print("")
+		print("---N O   S E   P U D O   L I M P I A R   L A   P A N T A L L A---")
+		print("")
+	else:
+		print("")
+		print("---E R R O R   D E S C O N O C I D O---")
+		print("Codigo de error: ", numero)
+		print("")
